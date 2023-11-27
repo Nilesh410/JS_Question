2891045d168e95ca0404b29bbf59b99622e09ebb
Nilesh410 <shirudenileshs@gmail.com>
2023-11-27 17:03:33 +0530

    add 12 question

---
+++ Q_12.js
@@ -0,0 +1,7 @@
+//Question 12. What is undefined x 1 in JavaScript
+var trees = ["redwood", "bay", "cedar", "oak", "maple"];
+console.log(trees,"\n","size of array:",trees.length)
+
+delete trees[3];
+console.log(trees,"\n","size of array:",trees.length)
+//delete operator just deinitialized the array element 
